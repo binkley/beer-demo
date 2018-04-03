6742562a243da9606864cc8a0c021608d5b4b56e
Brian Oxley <boxley@thoughtworks.com>
2018-04-03 08:39:27 -0500

    Beer list component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,48 +1,11 @@
 import * as React from 'react';
 import './App.css';
+import BeerList from './BeerList';
 
 const logo = require('./logo.svg');
 
-interface Beer {
-  id: number;
-  name: string;
-}
-
-interface AppState {
-  beers: Array<Beer>;
-  isLoading: boolean;
-}
-
-class App extends React.Component<{}, AppState> {
-  constructor(props: {}) {
-    super(props);
-
-    this.state = {
-      beers: [],
-      isLoading: false
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      isLoading: true
-    });
-
-    fetch('http://localhost:8080/good-beers')
-      .then(response => response.json())
-      .then(data => this.setState({
-        beers: data,
-        isLoading: false
-      }));
-  }
-
+class App extends React.Component<{}, {}> {
   render() {
-    const {beers, isLoading} = this.state;
-
-    if (isLoading) {
-      return <p>Loading...</p>;
-    }
-
     return (
       <div className="App">
         <header className="App-header">
@@ -50,14 +13,7 @@ class App extends React.Component<{}, AppState> {
           <h1 className="App-title">Welcome to React</h1>
         </header>
 
-        <div>
-          <h2>Beers</h2>
-          {beers.map((beer: Beer) => {
-            return <div key={beer.id}>
-              {beer.name}
-            </div>;
-          })}
-        </div>
+        <BeerList/>
 
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to
---
+++ client/src/BeerList.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import './App.css';
+
+interface Beer {
+  id: number;
+  name: string;
+}
+
+interface BeerListState {
+  beers: Array<Beer>;
+  isLoading: boolean;
+}
+
+class BeerList extends React.Component<{}, BeerListState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      beers: [],
+      isLoading: false
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      isLoading: true
+    });
+
+    fetch('http://localhost:8080/good-beers')
+      .then(response => response.json())
+      .then(data => this.setState({
+        beers: data,
+        isLoading: false
+      }));
+  }
+
+  render() {
+    const {beers, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <div>
+        <h2>Beers</h2>
+        {beers.map((beer: Beer) => {
+          return <div key={beer.id}>
+            {beer.name}
+          </div>;
+        })}
+      </div>
+    );
+  }
+}
+
+export default BeerList;
