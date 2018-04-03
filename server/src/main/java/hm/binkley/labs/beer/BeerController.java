e59fbc0f9fc2b01a3a3d166660b7a4c9093a69ce
Brian Oxley <boxley@thoughtworks.com>
2018-04-03 07:47:36 -0500

    RESTy

---
+++ server/src/main/java/hm/binkley/labs/beer/BeerController.java
@@ -0,0 +1,24 @@
+package hm.binkley.labs.beer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Stream;
+
+@RequiredArgsConstructor
+@RestController
+public final class BeerController {
+    private final BeerRepository repository;
+
+    private static boolean isGreat(final Beer beer) {
+        return !"Budweiser".equals(beer.getName()) && !"Coors Light"
+                .equals(beer.getName());
+    }
+
+    @GetMapping("/good-beers")
+    public Stream<Beer> goodBeers() {
+        return repository.findAll().stream().
+                filter(BeerController::isGreat);
+    }
+}
