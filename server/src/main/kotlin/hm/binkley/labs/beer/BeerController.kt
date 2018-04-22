8aeaed2f9b6c5077febc2e46b328f78fabf83948
Brian Oxley <boxley@thoughtworks.com>
2018-04-22 05:49:19 -0400

    Fix Spring injection

--- server/src/main/kotlin/hm/binkley/labs/beer/BeerController.kt
+++ server/src/main/kotlin/hm/binkley/labs/beer/BeerController.kt
@@ -1,14 +1,13 @@
 package hm.binkley.labs.beer
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.stream.Stream
 
 @RestController
-class BeerController {
-    private val repository: BeerRepository? = null
-
+class BeerController(@Autowired private val repository: BeerRepository) {
     private fun isGreat(beer: Beer): Boolean {
         return "Budweiser" != beer.name && "Coors Light" != beer.name
     }
@@ -16,6 +15,6 @@ class BeerController {
     @GetMapping("/good-beers")
     @CrossOrigin(origins = ["http://localhost:3000", "http://localhost:5000"])
     fun goodBeers(): Stream<Beer> {
-        return repository!!.findAll().stream().filter { isGreat(it) }
+        return repository.findAll().stream().filter { isGreat(it) }
     }
 }
