f8aeb1f96c1d2b36e655470d70431e6c7f4160e9
Brian Oxley <boxley@thoughtworks.com>
2018-04-23 06:00:13 -0400

    QueryDSL

--- server/src/main/kotlin/hm/binkley/labs/beer/BeerRepository.kt
+++ server/src/main/kotlin/hm/binkley/labs/beer/BeerRepository.kt
@@ -1,13 +1,15 @@
 package hm.binkley.labs.beer
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.querydsl.QuerydslPredicateExecutor
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.data.rest.core.annotation.RestResource
 import org.springframework.web.bind.annotation.CrossOrigin
 
 @CrossOrigin
 @RepositoryRestResource
-interface BeerRepository : JpaRepository<Beer, Long> {
+interface BeerRepository : JpaRepository<Beer, Long>,
+        QuerydslPredicateExecutor<Beer> {
     @RestResource(path = "good-beers", rel = "good-beers")
     fun findByQualityGreaterThan(quality: Int): List<Beer>
 }
